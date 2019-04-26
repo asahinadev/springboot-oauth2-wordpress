087b3f8c4195fabd3a3480160c1a397e08985262
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-04-27 00:47:10 +0900

    カスタム設定のミニマム化

--- src/main/java/com/example/spring/wordpress/config/SecurityConfiguig.java
+++ src/main/java/com/example/spring/wordpress/config/SecurityConfiguig.java
@@ -1,25 +1,14 @@
 package com.example.spring.wordpress.config;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.DelegatingOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
-import com.example.spring.wordpress.oauth2.service.WordpressUserService;
-import com.example.spring.wordpress.oauth2.token.AccessTokenResponseClient;
+import com.example.spring.wordpress.oauth2.user.WordpressUser;
 
 @Configuration
 @EnableWebSecurity
@@ -27,91 +16,40 @@ public class SecurityConfiguig
 		extends WebSecurityConfigurerAdapter {
 
 	@Override
-	public void configure(WebSecurity web)
-			throws Exception {
-		super.configure(web);
-
-		web.ignoring().antMatchers(
-				// webjars
-				"/webjars/**",
-				// CSS ファイル
-				"/css/**",
-				// JavaScriptファイル
-				"/js/**",
-				// 画像ファイル
-				"/img/**",
-				// サウンドファイル
-				"/sound/**",
-				// WEB フォント
-				"/font/**",
-				"/fonts/**",
-				// 外部ライブラリ
-				"/exlib/**"
-		/**/
-		);
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/webjars/**", "/assets/**");
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		super.configure(auth);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http)
 			throws Exception {
-		super.configure(http);
+		http
+				// 認証情報
+				.authorizeRequests()
+				.anyRequest().authenticated().and()
 
-		http.formLogin().disable();
-		http.logout().disable();
+				// 認証は SSO オンリー
+				.httpBasic().disable()
+				.formLogin().disable()
+				.logout().disable()
 
-		http.httpBasic().disable();
-
-		http.csrf().disable();
-
-		http.oauth2Login()
-
-				// 認証エンドポイント
-				.authorizationEndpoint()
-				.and()
-
-				// リダイレクトエンドポイント
-				.redirectionEndpoint()
-				.and()
-
-				.tokenEndpoint()
-				.accessTokenResponseClient(new AccessTokenResponseClient())
-				.and()
+				// 認証関連ページは CSRF 対象外
+				.csrf().ignoringAntMatchers(
+						"/login",
+						"/login/**",
+						"/sso/**",
+						"/oauth/**",
+						"/oauth1/**",
+						"/oauth2/**")
+				.csrfTokenRepository(new HttpSessionCsrfTokenRepository()).and()
 
 				// ユーザー情報エンドポイント
-				.userInfoEndpoint()
-				// see this.userService()
-				// .customUserType(WordpressUser.class, "dropbox")
-				.userService(userService())
-				.and()
-
-		;
-
+				.oauth2Login().userInfoEndpoint()
+				.customUserType(WordpressUser.class, "wordpress");
 	}
-
-	private DelegatingOAuth2UserService<OAuth2UserRequest, OAuth2User> userService() {
-		Map<String, Class<? extends OAuth2User>> customUser = new HashMap<>();
-
-		List<OAuth2UserService<OAuth2UserRequest, OAuth2User>> userServices = new ArrayList<>();
-
-		// Wordpress 専用
-		userServices.add(new WordpressUserService());
-
-		// Custom UserService 
-		if (customUser.isEmpty() == false) {
-			userServices.add(new CustomUserTypesOAuth2UserService(customUser));
-		}
-
-		// Default UserService
-		userServices.add(new DefaultOAuth2UserService());
-
-		// 作成
-		return new DelegatingOAuth2UserService<>(userServices);
-	}
-
 }
--- src/main/java/com/example/spring/wordpress/oauth2/user/WordpressUser.java
+++ src/main/java/com/example/spring/wordpress/oauth2/user/WordpressUser.java
@@ -1,9 +1,12 @@
 package com.example.spring.wordpress.oauth2.user;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,6 +15,8 @@ import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nimbusds.oauth2.sdk.token.AccessToken;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,30 +26,118 @@ import lombok.Setter;
 public class WordpressUser
 		implements OAuth2User {
 
+	@JsonIgnore
+	AccessToken accessToken;
+
 	@JsonAnySetter
 	Map<String, Object> extraParameters = new HashMap<>();
 
-	@JsonIgnore
-	public String getId() {
-		return String.valueOf(getExtraParameters().get("ID"));
-	}
+	@JsonProperty(META)
+	Map<String, Map<String, String>> meta = new HashMap<>();
 
-	public String getDisplayName() {
-		return String.valueOf(getExtraParameters().get("display_name"));
-	}
+	@JsonProperty(TOKEN_SCOPE)
+	Set<String> tokenScope = new HashSet<>();
 
-	public String getUsername() {
-		return String.valueOf(getExtraParameters().get("username"));
-	}
+	@JsonProperty(ABTESTS)
+	Map<String, Object> abtests = new HashMap<>();
 
-	public String getEmail() {
-		return String.valueOf(getExtraParameters().get("email"));
-	}
+	@JsonProperty(SOCIAL_LOGIN_CONNECTIONS)
+	Set<String> socialLoginConnections = new HashSet<>();
+
+	public static final String ID = "ID";
+	public static final String DISPLAY_NAME = "display_name";
+	public static final String USERNAME = "username";
+	public static final String EMAIL = "email";
+	public static final String PRIMARY_BLOG = "primary_blog";
+	public static final String PRIMARY_BLOG_URL = "primary_blog_url";
+	public static final String PRIMARY_BLOG_IS_JETPACK = "primary_blog_is_jetpack";
+	public static final String LANGUAGE = "language";
+	public static final String LOCALE_VARIANT = "locale_variant";
+	public static final String TOKEN_SITE_ID = "token_site_id";
+	public static final String TOKEN_SCOPE = "token_scope";
+	public static final String AVATAR_URL = "avatar_URL";
+	public static final String PROFILE_URL = "profile_URL";
+	public static final String VERIFIED = "verified";
+	public static final String EMAIL_VERIFIED = "email_verified";
+	public static final String DATE = "date";
+	public static final String SITE_COUNT = "site_count";
+	public static final String VISIBLE_SITE_COUNT = "visible_site_count";
+	public static final String HAS_UNSEEN_NOTES = "has_unseen_notes";
+	public static final String NEWEST_NOTE_TYPE = "newest_note_type";
+	public static final String PHONE_ACCOUNT = "phone_account";
+
+	public static final String META = "meta";
+	public static final String META__LINKS = "links";
+	public static final String META__LINKS__SELF = "self";
+	public static final String META__LINKS__HELP = "help";
+	public static final String META__LINKS__SITE = "site";
+	public static final String META__LINKS__FLAGS = "flags";
+
+	public static final String IS_VALID_GOOGLE_APPS_COUNTRY = "is_valid_google_apps_country";
+	public static final String USER_IP_COUNTRY_CODE = "user_ip_country_code";
+	public static final String SOCIAL_LOGIN_CONNECTIONS = "social_login_connections";
+	public static final String SOCIAL_SIGNUP_SERVICE = "social_signup_service";
+	public static final String ABTESTS = "abtests";
 
 	@Override
 	@JsonIgnore
 	public String getName() {
-		return getId();
+		return extraParameters.get(ID).toString();
+	}
+
+	@JsonIgnore
+	public String getEmail() {
+		return extraParameters.get(EMAIL).toString();
+	}
+
+	@JsonIgnore
+	public Set<String> getTokenScope() {
+		if (tokenScope == null) {
+			return new HashSet<>();
+		}
+		return tokenScope;
+	}
+
+	@JsonIgnore
+	public Map<String, Map<String, String>> getMeta() {
+		if (meta == null) {
+			return new HashMap<>();
+		}
+		return meta;
+	}
+
+	@JsonIgnore
+	public Map<String, String> getMetaLinks() {
+		String key = META__LINKS;
+		if (getMeta().get(key) == null) {
+			return new HashMap<>();
+		}
+		return getMeta().get(key);
+	}
+
+	@JsonIgnore
+	public String getMetaLinks(String key) {
+		return getMetaLinks(key);
+	}
+
+	@JsonIgnore
+	public Map<String, Object> getAbtests() {
+		if (abtests == null) {
+			return new HashMap<>();
+		}
+		return abtests;
+	}
+
+	@JsonIgnore
+	public Object getAbtests(String key) {
+		return getAbtests().get(key);
+	}
+
+	public Set<String> getSocialLoginConnections() {
+		if (socialLoginConnections == null) {
+			return new HashSet<>();
+		}
+		return socialLoginConnections;
 	}
 
 	@Override
@@ -58,11 +151,18 @@ public class WordpressUser
 	@Override
 	@JsonIgnore
 	public Map<String, Object> getAttributes() {
-		return this.getExtraParameters();
+		Map<String, Object> attributes = new HashMap<>(extraParameters);
+
+		attributes.put(META, getMeta());
+		attributes.put(TOKEN_SCOPE, getTokenScope());
+		attributes.put(ABTESTS, getAbtests());
+		attributes.put(SOCIAL_LOGIN_CONNECTIONS, getSocialLoginConnections());
+
+		return Collections.unmodifiableMap(attributes);
 	}
 
 	public String toString() {
-		return getExtraParameters().toString();
+		return getAttributes().toString();
 	}
 
 }
