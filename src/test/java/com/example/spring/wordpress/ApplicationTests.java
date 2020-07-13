6f39089d4c1f2c82238049bf927fde2975fdb738
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-13 23:45:58 +0900

    Spring Boot 2.3.1 & gradle

--- src/test/java/com/example/spring/wordpress/ApplicationTests.java
+++ src/test/java/com/example/spring/wordpress/ApplicationTests.java
@@ -1,11 +1,8 @@
 package com.example.spring.wordpress;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
